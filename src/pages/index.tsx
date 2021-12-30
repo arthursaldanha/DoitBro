020c2e7433416caf9e979566eb984a37290ef4b2
Arthur Saldanha <saldanhadev@gmail.com>
2021-12-30 00:21:37 -0300

    Finalizando DoitBro

---
+++ src/pages/index.tsx
@@ -0,0 +1,68 @@
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+
+// Components
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+
+import styles from "../styles/pages/Home.module.css";
+
+import { CountdownProvider } from "../context/CountdownContext";
+import { ChallengerProvider } from "../context/ChallengesContext";
+
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home({
+  level,
+  currentExperience,
+  challengesCompleted,
+}: HomeProps) {
+  return (
+    <ChallengerProvider
+      level={level}
+      currentExperience={currentExperience}
+      challengesCompleted={challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | DoitBro</title>
+        </Head>
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengerProvider>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
+
+/* 38:10 */
